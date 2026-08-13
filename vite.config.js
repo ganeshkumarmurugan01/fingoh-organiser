eadae354724a3eea4ecba2f56ef3c69360a343e8
ganeshkumarmurugan01 <ganeshkumarmurugan01@gmail.com>
2026-08-13 15:39:06 +0530

    chore: scaffold fingoh-organiser

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://web-production-93e78d.up.railway.app',
+        changeOrigin: true,
+        secure: false,
+        configure: (proxy) => {
+          proxy.on('proxyReq', (proxyReq) => {
+            proxyReq.setHeader('X-Forwarded-Proto', 'https')
+          })
+        }
+      }
+    }
+  }
+})
